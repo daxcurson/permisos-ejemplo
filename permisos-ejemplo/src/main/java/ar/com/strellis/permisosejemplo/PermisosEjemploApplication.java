7627abfa9610eb1f5db9721585af4972adc666ea
Agustín Villafañe <daxcurson@users.noreply.github.com>
2022-07-23 19:37:48 -0300

    Comienza la migración a Spring boot!
    Comienza la migración de permisos-ejemplo a spring-boot

---
+++ permisos-ejemplo/src/main/java/ar/com/strellis/permisosejemplo/PermisosEjemploApplication.java
@@ -0,0 +1,13 @@
+package ar.com.strellis.permisosejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermisosEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermisosEjemploApplication.class, args);
+	}
+
+}
